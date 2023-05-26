2f3428e3e892415e50b043b95133a7921143a823
dmitrykhomich <d.mastazm@gmail.com>
2023-05-26 15:13:50 +0300

    Add method convertKmsToMiles

--- src/_01_app_start/Main.java
+++ src/_01_app_start/Main.java
@@ -4,8 +4,13 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Converter for base converting");
         System.out.println("Version 1.0");
-        }
+    }
+
     private static double convertMilesToKms(double miles) {
         return miles * 1.60934;
     }
+
+    private static double convertKmsToMiles(double kms) {
+        return kms / 1.60934;
     }
+}
